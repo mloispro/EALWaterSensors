1899fb897822d84ccd9888a42432453e7c4b1d7d
mloispro <olson_mitchell@hotmail.com>
2016-11-19 07:05:32 -0600

    async delay

--- DIYTdsSensorTester/DIYTdsSensorTester.ino
+++ DIYTdsSensorTester/DIYTdsSensorTester.ino
@@ -1,4 +1,5 @@
 #include <LiquidCrystal.h>
+#include "AsyncDelay.h"
 
 // how many samples to take and average, more takes longer
 // but is more 'smooth'
@@ -15,7 +16,7 @@ float Volts = 5.0;
 
  //constants
 const int TEMP_MANUAL = 25; // temp. for nominal resistance (almost always 25 C)
-const int SAMPLING_PERIOD = 2; // the number of seconds to measure 555 oscillations
+const int SAMPLING_PERIOD = 2; //*DONT EVER CHANGE *affects reading*, the number of seconds to measure 555 oscillations
 const long THERMISTOR_NOMINAL = 10000; //*DONT EVER CHANGE resistance at 25 degrees C
 const long SERIES_RESISTOR = 10000;  //*DONT EVER CHANGE the value of the 'other' resistor     
 const long BETA_COEFFICIENT = 3950; // The beta coefficient of the thermistor (usually 3000-4000)
@@ -30,6 +31,7 @@ float Temp, TempF, TDS;
 
 //lcd
 LiquidCrystal _lcd(8, 9, 4, 5, 6, 7); 
+AsyncDelay _asyncDelay(SAMPLING_PERIOD*1000, 1000);
 
 void setup()
 {
@@ -40,26 +42,96 @@ void setup()
   
   // Print a message to the LCD.
   _lcd.print("TDS Tester");
-           
+  
+  _asyncDelay.Start(startReading);
+  _asyncDelay.Stop(stopReading);
 }
 
-void lcdPrintLine(int lineNum, String text){
-  _lcd.setCursor(0, lineNum);
-  // print the number of seconds since reset:
-  _lcd.print(text);
-}
+
 
 void loop()
 {
-  getReadings();
+  _asyncDelay.Loop();
+  //Serial.println("looping " + String(_interrupter.MilliSecRemaining));
+  //delay(100);
+}
+
+void startReading(){
+  Serial.println("startReading ");
+        //turn on the 555 system
+    digitalWrite(_tdsPowerPin,HIGH); //turns on the 555 timer subcircuit
+    
+    getTempurature();
+    
+    // conductivity --------------------------------------
+     
+    _pulseCount=0; //reset the pulse counter
+    _totalDuration=0;  //reset the totalDuration of all pulses measured
+    
+    //start sampling
+    attachInterrupt(digitalPinToInterrupt(_tdsPin),onPulse,RISING); //attach an interrupt counter to interrupt pin 1 (digital pin #3) -- the only other possible pin on the 328p is interrupt pin #0 (digital pin #2)
+    //attachInterrupt(1,onPulse,RISING);
+    
+    _pulseTime=micros(); // start the stopwatch
+}
+
+void stopReading(){
+   Serial.println("stopReading ");
+   detachInterrupt(digitalPinToInterrupt(_tdsPin)); //we've finished sampling, so detach the interrupt function -- don't count any more pulses
+    //detachInterrupt(1);
+  
+    //turn off the 555 system
+    digitalWrite(_tdsPowerPin,LOW);
+  
+    if (_pulseCount>0) { //use this logic in case something went wrong
+    
+    double durationS=_totalDuration/double(_pulseCount)/1000000.; //the total duration, in seconds, per pulse (note that totalDuration was in microseconds)
+    
+    // print out stats
+    /*Serial.print("sampling period=");
+    Serial.print(samplingPeriod);
+      Serial.print(" sec; #pulses=");
+    Serial.print(pulseCount);
+    Serial.print("; duration per pulse (sec)=");
+    */
+    
+    // print conductivity value
+//    Serial.print(durationS,4);
+//    Serial.print(", ");
+//    Serial.println(Temp);
+      
+    }
+    
+    TempF = convTempToFahrenheit(Temp);
+    calculateTDS();
+
+    //should be called from controller in real app.
+    printValsToSerial();
+    printValsToLCD();
+}
+
+void onPulse()
+{
+ 
+  _pulseCount++;
+//  Serial.print("pulsecount=");
+//  Serial.println(pulseCount);
+  _lastTime = _pulseTime;
+  _pulseTime = micros();
+  _duration=_pulseTime-_lastTime;
+  _totalDuration+=_duration;
+  //Serial.println(totalDuration);
 }
 
 void printValsToLCD(){
-    
 
+    _lcd.clear();
     String tdsString = "TDS:" + String(TDS, 0)+ "," + String(TempF, 1) + "F";
-    
     lcdPrintLine(0, tdsString);
+
+    //todo: remove this:
+    String cnt = "cnt:" + String(_pulseCount);
+    lcdPrintLine(1, cnt);
 }
 
 void printValsToSerial(){
@@ -78,21 +150,16 @@ void printValsToSerial(){
   Serial.print(_pulseCount);
   Serial.println(" Hz");
 }
+void lcdPrintLine(int lineNum, String text){
+  _lcd.setCursor(0, lineNum);
+  // print the number of seconds since reset:
+  _lcd.print(text);
+}
+void getTempurature(){
+      // temperature ---------------------------------------------
 
-void getReadings(){
-
-  static unsigned long samplingTime = millis();
-
-  long waitTime = SAMPLING_PERIOD * 1000 + 500;//wait .5 sec between readings, according to spec
-  if(millis() - samplingTime > waitTime) {
-
-      // now make a conductivity measurement
-    float average;
-    
-      //turn on the 555 system
-    digitalWrite(_tdsPowerPin,HIGH); //turns on the 555 timer subcircuit
-    
-    // temperature ---------------------------------------------
+  float average;
+      
     // take N samples in a row, with a slight delay
     for (int i=0; i< NUMSAMPLES; i++) {
      int tempReading = analogRead(_temperaturePin);
@@ -126,88 +193,33 @@ void getReadings(){
    Temp = steinhart;
   //  Serial.print(Temp);
   //  Serial.println(" *C");
-    
-    
-    // conductivity --------------------------------------
-     
-    _pulseCount=0; //reset the pulse counter
-    _totalDuration=0;  //reset the totalDuration of all pulses measured
-    
-    
-    attachInterrupt(digitalPinToInterrupt(_tdsPin),onPulse,RISING); //attach an interrupt counter to interrupt pin 1 (digital pin #3) -- the only other possible pin on the 328p is interrupt pin #0 (digital pin #2)
-    //attachInterrupt(1,onPulse,RISING);
-    
-    _pulseTime=micros(); // start the stopwatch
-
-    //todo: change to if
-    delay(SAMPLING_PERIOD*1000); //give ourselves samplingPeriod seconds to make this measurement, during which the "onPulse" function will count up all the pulses, and sum the total time they took as 'totalDuration' 
-   
-    detachInterrupt(digitalPinToInterrupt(_tdsPin)); //we've finished sampling, so detach the interrupt function -- don't count any more pulses
-    //detachInterrupt(1);
-    
-    //turn off the 555 system
-    digitalWrite(_tdsPowerPin,LOW);
-    
-    if (_pulseCount>0) { //use this logic in case something went wrong
-    
-    double durationS=_totalDuration/double(_pulseCount)/1000000.; //the total duration, in seconds, per pulse (note that totalDuration was in microseconds)
-    
-    // print out stats
-    /*Serial.print("sampling period=");
-    Serial.print(samplingPeriod);
-      Serial.print(" sec; #pulses=");
-    Serial.print(pulseCount);
-    Serial.print("; duration per pulse (sec)=");
-    */
-    
-    // print conductivity value
-//    Serial.print(durationS,4);
-//    Serial.print(", ");
-//    Serial.println(Temp);
-      
-    }
-    
-    TempF = convTempToFahrenheit(Temp);
-    calculateTDS();
-    printValsToSerial();
-    printValsToLCD();
-    
-    samplingTime = millis();
-  }
-  
 }
-
-void onPulse()
-{
- 
-  _pulseCount++;
-//  Serial.print("pulsecount=");
-//  Serial.println(pulseCount);
-  _lastTime = _pulseTime;
-  _pulseTime = micros();
-  _duration=_pulseTime-_lastTime;
-  _totalDuration+=_duration;
-  //Serial.println(totalDuration);
-}
-
 
 const float noEC = 0.00;   //  The conductivity probe is dry
-const float lowEC = 2.00;   //  Value of calibration buffers
-const float medEC = 12.88;  //  You can use any other buffers
-const float medHighEC = 60.00;
-const float highEC = 80.00;  
+const float lowEC = 6.00;//2.00   //  Value of calibration buffers
+const float medEC = 46.0;//12.88;  //  You can use any other buffers
+const float highEC = 76.00;//80.0;
+const float highHighEC = 142.00;  
 
 //calibration vars, these change after calibration
-int noPulse = 230;
-int lowPulse = 1340;
-int medPulse = 4140;
-int medHighPulse = 5100;
-int highPulse = 7350;
+const int noPulse = 230;
+const int lowPulse = 1340;
+const int medPulse = 4140;
+const int highPulse = 6350;//7350;
+const int highHighPulse = 10200;
 
-//pen   diy   lvl   match
+//pen   diy   cnt     lvl   match
 //-------------------------
-//90    70    high  no       <-need intermediate level
-//180   180   high  yes
+//30          2420
+//52          3848
+//70          4939
+//94    70    6722   high  no       <-need intermediate level
+//130         8770
+//180   180   11080   high  yes
+//220   226   12260
+//280   300   15300
+//310   330   16370
+//440   447   21000    
 
 void calculateTDS() {  //  Calculate the measurement
 
@@ -217,63 +229,60 @@ void calculateTDS() {  //  Calculate the measurement
 
   long lowPulseCount = (noPulse + lowPulse);
   long medPulseCount = (medPulse + lowPulse + noPulse);
-  long medHighPulseCount = (medHighPulse + medPulse + lowPulse + noPulse);
-
+  long highHighPulseCount = (highPulse + medPulse + lowPulse + noPulse);
+ 
   int higherPulseVar;
   int lowerPulseVar;
   float higherEcVar;
   float lowerEcVar;
   
-  if (_pulseCount >= noPulse && _pulseCount < lowPulseCount) { //low
-    Serial.println("calc case - low");
-    higherPulseVar = lowPulse;
-    lowerPulseVar = noPulse;
-    higherEcVar = lowEC;
-    lowerEcVar = noEC;
-  }
-  else if (_pulseCount >= lowPulseCount && _pulseCount < medPulseCount) { //med
-    Serial.println("calc case - med");
-    higherPulseVar = medPulse;
-    lowerPulseVar = lowPulse;
-    higherEcVar = medEC;
-    lowerEcVar = lowEC;
-  }
-  else if (_pulseCount >= medPulseCount && _pulseCount < medHighPulseCount) { //med high
-    Serial.println("calc case - med high");
-    higherPulseVar = medHighPulse;
-    lowerPulseVar = medPulse;
-    higherEcVar = medHighEC;
-    lowerEcVar = medEC;
-  }
-  else if (_pulseCount >= medHighPulseCount) { //high
-    Serial.println("calc case - high");
-    higherPulseVar = highPulse;
-    lowerPulseVar = medPulse;
-    higherEcVar = highEC;
-    lowerEcVar = medEC;
-//     higherPulseVar = highPulse;
-//    lowerPulseVar = medHighPulse;
-//    higherEcVar = highEC;
-//    lowerEcVar = medHighEC;
-  }
-//  else if (_pulseCount >= medPulseCount) { //high
-//    Serial.println("calc case - high");
-//    a = (highPulse - medPulse) / (highEC - medEC);
-//    b = medPulse - (a * medEC);
-//    c = (_pulseCount - b) / a;
-//  }
+    if (_pulseCount >= noPulse && _pulseCount < lowPulseCount)
+    { //low
+        Serial.println("calc case - low");
+        higherPulseVar = lowPulse;
+        lowerPulseVar = noPulse;
+        higherEcVar = lowEC;
+        lowerEcVar = noEC;
+    }
+    else if (_pulseCount >= lowPulseCount && _pulseCount < medPulseCount)
+    { //med
+        Serial.println("calc case - med");
+        higherPulseVar = medPulse;
+        lowerPulseVar = lowPulse;
+        higherEcVar = medEC;
+        lowerEcVar = lowEC;
+    }
+    else if (_pulseCount >= medPulseCount && _pulseCount < highHighPulseCount)
+    { //high
+        Serial.println("calc case - high");
+        higherPulseVar = highPulse;
+        lowerPulseVar = medPulse;
+        higherEcVar = highEC;
+        lowerEcVar = medEC;
+    }
+    else if (_pulseCount >= highHighPulseCount)
+    { //highhigh
+        Serial.println("calc case - highhigh");
+        higherPulseVar = highHighPulse;
+        lowerPulseVar = highPulse;
+        higherEcVar = highHighEC;
+        lowerEcVar = highEC;
+    }
 
-  if(_pulseCount > noPulse){
-    a = (higherPulseVar - lowerPulseVar) / (higherEcVar - lowerEcVar);
-    b = lowerPulseVar - (a * lowerEcVar);
-    c = (_pulseCount  - b) / a;
-  }
-  else{
-    Serial.println("calc case -none");
-    c=0;
-  }
+    if (_pulseCount > noPulse)
+    {
+        a = (higherPulseVar - lowerPulseVar) / (higherEcVar - lowerEcVar);
+        b = lowerPulseVar - (a * lowerEcVar);
+        c = (_pulseCount - b) / a;
+    }
+    else
+    {
+        Serial.println("calc case - none");
+        c = 0;
+    }
 
-  TDS = (c / (1 + ALPHA_LTC * (Temp - 25.00))); //  temperature compensation
+    TDS = (c / (1 + ALPHA_LTC * (Temp - 25.00))); //  temperature compensation
+
 }
 double convTempToFahrenheit(double temp) {
     float vIn = Volts;//3.1;//5;
